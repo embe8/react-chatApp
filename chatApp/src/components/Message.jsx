6f13f1b7b5d00f932e1c7e4a7067fcc6c6f4ed5c
Erika E <137976326+embe8@users.noreply.github.com>
2025-09-17 16:23:43 -0700

    Update Message.jsx
    Add Authcontext and Chatcontext

--- chatApp/src/components/Message.jsx
+++ chatApp/src/components/Message.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
 import userImage from '../img/capybara-square-1.jpg.optimal.jpg'
-
-const Message = () =>{
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+const Message = ({message}) =>{
+    const {currentUser} = useContext(AuthContext);
+    const {data} = useContext(ChatContext);
     return(
         <div className='message owner'>
             <div className='messageInfo'>
