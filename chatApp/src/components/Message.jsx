4c23895e60da071fa0d842dfb8bba4fae4850d17
Erika E <137976326+embe8@users.noreply.github.com>
2025-09-17 16:24:11 -0700

    Update Message.jsx
    Add Authcontext and Chatcontext

--- chatApp/src/components/Message.jsx
+++ chatApp/src/components/Message.jsx
@@ -1,22 +1,33 @@
-import userImage from '../img/capybara-square-1.jpg.optimal.jpg'
+import Message from "./Message";
+import { doc, onSnapshot } from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
-const Message = ({message}) =>{
-    const {currentUser} = useContext(AuthContext);
-    const {data} = useContext(ChatContext);
-    return(
-        <div className='message owner'>
-            <div className='messageInfo'>
-                <img src={userImage} alt=""></img>
-                <span>just now</span>
-                </div>
-                <div className='messageContent'>
-                    <p>hello</p>
-                    <img src={userImage} alt="" />
-                </div>
-            </div>
-    )
-}
+import { db } from "../firebase";
 
-export default Message
+const Messages = () =>{
+    const [messages, setMessages] = useState([]);
+    const { data } = useContext(ChatContext);
+
+    useEffect(()=>{
+        const unSub = onSnapshot(doc(db,"chats", data.chatId), (doc)=>{
+            doc.exists() && setMessages(doc.data().messages);
+        });
+
+        //cleanup
+        return ()=>{
+            unSub()
+        }
+    },[data.chatId])
+
+    return(
+        <div className='messages'>
+            {messages.map((m) =>(
+
+                <Message message={m} key={m.id}/>
+            ))}
+
+        </div>
+    );
+};
+
+export default Messages
