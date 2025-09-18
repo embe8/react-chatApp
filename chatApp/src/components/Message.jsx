bebf27c68d7311e499fb9313574fa6a967703c8a
Erika E <137976326+embe8@users.noreply.github.com>
2025-09-17 20:26:15 -0700

    Update Message.jsx
    Added smooth effect using useEffect to sidebar message status for messages received

--- chatApp/src/components/Message.jsx
+++ chatApp/src/components/Message.jsx
@@ -1,19 +1,27 @@
 import userImage from '../img/capybara-square-1.jpg.optimal.jpg'
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 const Message = ({message}) =>{
     const {currentUser} = useContext(AuthContext);
     const {data} = useContext(ChatContext);
+
+    const ref = useRef();
+
+    useEffect(() => {
+        ref.current?.scrollIntoView({ behavior: "smooth"});
+    }, [message]);
     return(
-        <div className='message owner'>
+        <div 
+        ref={ref}
+        className={`message ${message.senderId === currentUser.uid && "owner"}`}>
             <div className='messageInfo'>
-                <img src={userImage} alt=""></img>
+                <img src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt=""></img>
                 <span>just now</span>
                 </div>
                 <div className='messageContent'>
-                    <p>hello</p>
-                    <img src={userImage} alt="" />
+                    <p>{message.text}</p>
+                    {message.img && <img src={message.img} alt="" />}
                 </div>
             </div>
     )
