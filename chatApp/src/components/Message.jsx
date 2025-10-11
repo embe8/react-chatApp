c159d1b077ab7d72eee2d227cdfb74e4078f2b2f
Erika E <137976326+embe8@users.noreply.github.com>
2025-10-11 12:29:11 -0700

    Update Message.jsx

--- chatApp/src/components/Message.jsx
+++ chatApp/src/components/Message.jsx
@@ -1,4 +1,3 @@
-import userImage from '../img/capybara-square-1.jpg.optimal.jpg'
 import React, { useContext, useEffect, useRef, useState} from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
