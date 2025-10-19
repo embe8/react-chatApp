5fa14d1b9fc504b16206824f901f6279c2124c6e
Erika E <137976326+embe8@users.noreply.github.com>
2025-10-19 10:36:41 -0700

    Update Chat.jsx

--- chatApp/src/components/Chat.jsx
+++ chatApp/src/components/Chat.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState, useContext} from 'react'
 import Cam from "../img/cam.png";
 import Add from "../img/add.png";
 import More from "../img/more.png";
 import Messages from "./Messages";
 import Input from "./Input";
+import { ChatContext } from '../context/ChatContext';
 
 const Chat = () =>{
+
+    const { data } = useContext(ChatContext);
     return(
         <div className="chat">
         <div className="chatInfo">
-            <span>Jane</span>
+            <span>{data.user?.displayName}</span>
             <div className="chatIcons">
-            <img src={Cam} alt=""></img>
+            {/* uncomment when functionality added<img src={Cam} alt=""></img>
             <img src={Add} alt=""></img>
-            <img src={More} alt=""></img>
+            <img src={More} alt=""></img>*/}
             </div>
           
         </div>
