243232460d7a2e520e098528c48f63011fdb20ff
embe8 <eebon@icloud.com>
2025-10-19 14:34:17 -0700

    fix: initialize currentUser as null to resolve login page loading issue

--- chatApp/src/context/AuthContext.jsx
+++ chatApp/src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ import { onAuthStateChanged } from "firebase/auth";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState({});
+  const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {//checks if there's a user and sets the user if there is
